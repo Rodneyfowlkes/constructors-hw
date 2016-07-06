13fba758904f017fce711cf41cab3039abc5cd20
Rodney Fowlkes <Fowlkesrodney@gmail.com>
2016-07-05 23:36:06 -0400

    Unit testing

--- src/js/constructors/dog.js
+++ src/js/constructors/dog.js
@@ -1,3 +1,11 @@
 function Dog () {
-  
+this.status= 'normal',
+this.color = 'black',
+ this.hungry =  'true'
 };
+
+console.log('hello');
+export{ Dog }
+
+
+
--- src/js/constructors/person.js
+++ src/js/constructors/person.js
@@ -1,3 +1,8 @@
 function Person () {
-  
+  this.pet = function(x){x.status = 'happy';};
+  this.feed = function(x){x.hungry = 'false';};
+  this.cool = false
+
 };
+
+export {Person};
